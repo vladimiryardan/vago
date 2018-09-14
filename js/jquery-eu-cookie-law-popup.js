37c32c635119325bc455b07a4e79fef6856866eb
LAPTOP-A8LK12VR\redhot <vladimiryardan@gmail.com>
2018-09-15 06:04:48 +0800

    remove console.log in js of cookie pop out

--- js/jquery-eu-cookie-law-popup.js
+++ js/jquery-eu-cookie-law-popup.js
@@ -261,7 +261,7 @@ $(document).ready( function() {
 });
 
 $(document).bind("user_cookie_consent_changed", function(event, object) {
-	console.log("User cookie consent changed: " + $(object).attr('consent') );
+	//console.log("User cookie consent changed: " + $(object).attr('consent') );
 });
 
 }(jQuery));
